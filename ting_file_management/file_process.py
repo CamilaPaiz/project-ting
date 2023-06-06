0eee092682647e0e4f8b9b2fed03c7884253b6d3
CamilaPaiz <camilapaiz@hotmail.com>
2023-06-06 19:11:24 -0300

    feat:implementa função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,27 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    """processa arquivo e gera dicionário"""
+    # ignorar arquivos que já tenham sido processados anteriormente
+    for path in instance._data:
+        if path == path_file:
+            return
+
+    lines = txt_importer(path_file)
+    if lines is not None:
+        data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines,
+        }
+        instance.enqueue(path_file)
+        print(data)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    """remove arquivo"""
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    """Apresenta as informações superficiais processadas"""
